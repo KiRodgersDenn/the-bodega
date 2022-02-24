0198efc3d8778bc89959f1036052bdc519a2457b
Kiyomi Rodgers Denniston <kiyomi.rodgersdenniston@gmail.com>
2022-02-24 11:26:01 -0500

    data images

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,10 @@
 \c cta_dev;
 
 INSERT INTO plants (name, image, botanicalName, price, low_light) VALUES
-('Pothos'),
-('Monstera'),
-('Snake'),
-('Peace Lily'),
-('Aloe Vera'),
-('Ponytail Palm'),
-('Jade');
+('Pothos','https://d3gkbidvk2xej.cloudfront.net/images/products/v2/2794b44e-63d4-49f9-8c36-07cbcab48991.jpeg?version=1608581796.92290019800','Epipremmum Aureum',44,false),
+('Monstera','https://assets.weimgs.com/weimgs/rk/images/wcm/products/202204/0059/faux-5ft-monstera-plant-wide-mid-century-turned-wood-leg-p-c.jpg','Monstera deliciosa', 55, false),
+('Snake','https://cdn.apartmenttherapy.info/image/upload/v1591495166/gen-workflow/product-listing/snakeplantamz.jpg','Dracaena Trifasciata', 33, true),
+('Peace Lily','https://www.mydomaine.com/thmb/9aB79USSkhiCVgGyXh1TBSvM7v8=/900x900/filters:fill(auto,1)/1566417254329_20190821-1566417255317-b9314f1d9f7a4668a466c5ffb1913a8f.jpg', 'Spathiphyllum', 88, false),
+('Aloe Vera','https://www.plants.com/images/177111_20210218-1613669870200.jpg', 'Aloe Barbadensis Miller', 77, true),
+('Ponytail Palm','https://cdn.shopify.com/s/files/1/0253/6701/9565/products/Ponytail-Palm-Grow-Pot-Small_1000x1000.jpg?v=1586360312', 'Beaucarnea Recurvata', 22, false),
+('Jade','https://robertsonsflowers.imgix.net/images/itemVariation/jadeplantinnewportpot-200311121403.jpg','Crassula Ovata', 99, true);
